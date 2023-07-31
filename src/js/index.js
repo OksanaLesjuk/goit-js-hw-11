c5aa65cfa604db50b1bebd443d77d99415ef2e1e
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-31 18:15:48 +0300

    form

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+
+
+const form = document.querySelector('#search-form')
+form.addEventListener('submit', handlerSearch)
+// const btnSearch = 
+
+
+
+function handlerSearch(evt) {
+    evt.preventDefault();
+
+
+    if (evt.type === 'submit') {
+
+    };
+}
