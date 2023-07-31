7add209b81f5554423cc9c66b6df1207f2bef9d0
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-31 15:16:22 +0000

    Deploying to gh-pages from @ OksanaLesjuk/goit-js-hw-11@c5aa65cfa604db50b1bebd443d77d99415ef2e1e 🚀

---
+++ index.70e445f3.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),e.type}));
+//# sourceMappingURL=index.70e445f3.js.map
---
+++ index.a6a5b4a8.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),e.type}));
+//# sourceMappingURL=index.a6a5b4a8.js.map
