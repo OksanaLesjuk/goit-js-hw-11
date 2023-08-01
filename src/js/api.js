61e8399b0b74b5e740dcd147e36b2ae9c0610d36
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-08-01 13:13:16 +0300

    gallery

---
+++ src/js/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+const BAZE_URL = 'https://pixabay.com/api/'
+
+
+
+const getPhotosService = async (value) => {
+    try {
+        const { data } = await axios(BAZE_URL, {
+            params: {
+                key: '38571221-0f6f89c913260bc990fe2f825',
+                q: `${value}`,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: "true",
+                per_page: 40,
+            }
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { getPhotosService };
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,73 @@
-
+import { getPhotosService } from "./api";
 
 const form = document.querySelector('#search-form')
+const gallery = document.querySelector('.gallery')
+
 form.addEventListener('submit', handlerSearch)
 // const btnSearch = 
 
 
 
-function handlerSearch(evt) {
+async function handlerSearch(evt) {
     evt.preventDefault();
+
+    const { searchQuery } = evt.currentTarget.elements;
+
 
 
     if (evt.type === 'submit') {
+        try {
+            const getPhotos = await getPhotosService(searchQuery.value);
+            console.dir(getPhotos);
 
-    };
+            marcupGallery(getPhotos);
+
+
+
+        }
+
+
+        catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
+function marcupGallery({ hits }) {
+
+
+    const marcup = hits.map((
+        { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
+    ) => {
+        return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>${likes} Likes</b>
+          </p>
+          <p class="info-item">
+            <b>${views} Views</b>
+          </p>
+          <p class="info-item">
+            <b>${comments} Comments</b>
+          </p>
+          <p class="info-item">
+            <b>${downloads} Downloads</b>
+          </p>
+        </div>
+      </div>`
+    }).join(" ");
+
+
+
+    gallery.insertAdjacentHTML('beforeend', marcup)
+
 }
